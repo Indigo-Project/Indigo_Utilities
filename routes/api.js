ef3c0785876a128a6be948091ac9d0b58cf6c443
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-11-15 18:50:19 -0700

    updated deployed app directory references

--- routes/api.js
+++ routes/api.js
@@ -998,7 +998,7 @@ router.post("/batch-download", function(req, res, next) {
               console.log(userHome);
               console.log(process.env.NODE_ENV);
               if (process.env.NODE_ENV === "production") {
-                destination = userHome + '/Output_Files/Assessments/Indigo_Assessments_Tmp/' + "/" + lastName + ", " + firstName + suffix + ".pdf";
+                destination = userHome + '/Output_Files/Assessments/Indigo_Assessments_Tmp/' + lastName + ", " + firstName + suffix + ".pdf";
               } else if (process.env.NODE_ENV === "development_test" || process.env.NODE_ENV === "production") {
                 destination = userHome + '/Documents/IndigoProject/Indigo_Utilities/Output_Files/Assessments/Indigo_Assessments_Tmp/' + lastName + ", " + firstName + suffix + ".pdf";
                 var file = fs.createWriteStream(destination);
