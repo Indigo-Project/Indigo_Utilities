61a4bb2086b334f41e400445cca33e944b20711e
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2017-01-03 15:33:54 -0700

    redistributed angular services and cleaned up index.html

---
+++ public/app/services/Main_Service.js
@@ -0,0 +1,26 @@
+app.factory('Main_Service', ['$state', function($state) {
+
+  return {
+    accessFunction: function(selectedFunction) {
+      if (selectedFunction === "pbi_pfmt") {
+        $state.go("pbi_pfmt");
+      } else if (selectedFunction === "blue_list") {
+        $state.go("blue_list");
+      } else if (selectedFunction === "ent_list") {
+        $state.go("ent_list");
+      } else if (selectedFunction === "tti_massdl") {
+        $state.go("tti_massdl");
+      } else if (selectedFunction === "sum_page") {
+        $state.go("sum_page");
+      } else if (selectedFunction === "sum_stats") {
+        $state.go("sum_stats");
+      } else if (selectedFunction === "dashboard_gen") {
+        $state.go("dashboard_gen");
+      } else if (selectedFunction === "dashboard_manager"){
+        $state.go("dashboard_manager");
+      } else if (selectedFunction === "default"){
+        $state.go("default");
+      }
+    }
+  }
+}])
